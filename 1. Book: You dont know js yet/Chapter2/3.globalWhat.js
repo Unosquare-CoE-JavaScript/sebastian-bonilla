019ee841425b7b74871a4da4bf319be88cad26d0
Sebastian Bonilla <sebastian.bonilla@192.168.68.115>
2022-06-03 12:09:39 -0500

    Chapter2 Finished

---
+++ 1. Book: You dont know js yet/Chapter2/3.globalWhat.js
@@ -0,0 +1,8 @@
+function getStudentName() {
+    // assignment to an undeclared variable :(
+    nextStudent = "Suzy";
+}
+
+getStudentName();
+console.log(nextStudent);
+    // "Suzy" -- oops, an accidental-global variable!
