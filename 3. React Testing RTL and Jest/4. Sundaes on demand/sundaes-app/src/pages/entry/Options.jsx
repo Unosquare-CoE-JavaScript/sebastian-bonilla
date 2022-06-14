e220365e3695694e60e6261124b456c3f8c68210
Sebastian Bonilla <sebastian.bonilla@192.168.68.115>
2022-06-14 18:09:11 -0500

    Simulating server response with msm finished

---
+++ 3. React Testing RTL and Jest/4. Sundaes on demand/sundaes-app/src/pages/entry/Options.jsx
@@ -0,0 +1,38 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import ScoopOption from "./ScoopOption";
+import ToppingOption from "./ToppingOption";
+import Row from "react-bootstrap/Row";
+import AlertBanner from "../common/AlertBanner";
+
+export default function Options({ optionType }) {
+  const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
+
+  // optionType is 'scoops' or 'toppings'
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3030/${optionType}`)
+      .then((res) => setItems(res.data))
+      .catch((error) => {
+        setError(true);
+        // TODO: handle error response
+      });
+  }, [optionType]);
+
+  if (error) {
+    return <AlertBanner />;
+  }
+
+  const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
+
+  const optionItems = items.map((item) => (
+    <ItemComponent
+      key={item.name}
+      name={item.name}
+      imagePath={item.imagePath}
+    />
+  ));
+
+  return <Row>{optionItems}</Row>;
+}
