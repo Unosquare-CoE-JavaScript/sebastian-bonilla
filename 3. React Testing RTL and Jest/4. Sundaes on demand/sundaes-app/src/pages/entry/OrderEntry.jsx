4f785ac4d6a3c6d0d265bd91ac5a79e7d344eb20
Sebastian Bonilla <sebastian.bonilla@192.168.68.115>
2022-06-15 17:25:08 -0500

    Scoops subtotal with context

--- 3. React Testing RTL and Jest/4. Sundaes on demand/sundaes-app/src/pages/entry/OrderEntry.jsx
+++ 3. React Testing RTL and Jest/4. Sundaes on demand/sundaes-app/src/pages/entry/OrderEntry.jsx
@@ -3,6 +3,7 @@ import Options from "./Options";
 export default function OrderEntry() {
   return (
     <div>
+      holaaa
       <Options optionType="scoops" />
       <Options optionType="toppings" />
     </div>
